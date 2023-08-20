67684cd3030ad44f3b353f62ef8f6668d6d4ae08
mewbugger <wangleyan0735@163.com>
2023-08-20 17:25:34 +0800

    BootInNewIDE

--- src/pages/UserUpdatePage.vue
+++ src/pages/UserUpdatePage.vue
@@ -1,8 +1,8 @@
 <template>
   <template v-if="user">
-    <van-cell title="昵称" is-link to="/user/edit" :value="user.userName"  @click="toEdit('username', '昵称', user.userName)"/>
+    <van-cell title="昵称" is-link to="/user/edit" :value="user.userName"  @click="toEdit('userName', '昵称', user.userName)"/>
     <van-cell title="账号" :value="user.userAccount"/>
-    <van-cell title="头像" is-link to="/user/edit">
+    <van-cell title="头像" is-link to="/user/edit" @click="toEdit('avatarUrl', '头像', user.avatarUrl)">
       <img style="height: 48px" :src="user.avatarUrl"/>
     </van-cell>
     <van-cell title="性别" is-link :value="user.gender" @click="toEdit('gender', '性别', user.gender)"/>
@@ -15,21 +15,9 @@
 <script setup lang="ts">
 import {useRouter} from "vue-router";
 import {onMounted, ref} from "vue";
-import myAxios from "../plugins/myAxios";
-import {Toast} from "vant";
 import {getCurrentUser} from "../services/user";
 
-// const user = {
-//   id: 1,
-//   username: '鱼皮',
-//   userAccount: 'dogYupi',
-//   avatarUrl: 'https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/img/logo.png',
-//   gender: '男',
-//   phone: '123112312',
-//   email: '12345@qq.com',
-//   planetCode: '1234',
-//   createTime: new Date(),
-// }
+
 
 const user = ref();
 
